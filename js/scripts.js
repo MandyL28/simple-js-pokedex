132319c4e35b216a1b15b399820d1e4cee44e084
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-01-30 16:08:15 -0500

    Updated js file to include new script
    This is the new pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
-/* alert('Hello world');
+let name = 'Bulbasaur'; 
+let height = 5; 
+let type = 'Water'; 
+console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
 
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize); */
+let name = 'Pikachu'; 
+let height = 3; 
+let type = 'Electric'; 
+console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
 
-let favoriteFood = 'strawberry';
-document.write(favoriteFood);
+let name = 'Charizard'; 
+let height = 7; 
+let type = 'Fire'; 
+console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
+
+
+/*
+// array of strings
+let pokemonList = ['Bulbasaur', 'Pikachu', 'Charizard']; 
+console.log(2);
+                   
+           
+// height array, an array of arrays!
+let height = [Array(1), Array(1), Array(1)
+  [3],
+  [2],
+  [5]
+];
+console.log(height);
+
+
+
+// array of objects
+let type = [
+  { type: 'Water'},
+  { type: 'Lightning'},
+  { type: 'Fire'}
+  
+]; 
+ console.log(type); 
+
+*/
