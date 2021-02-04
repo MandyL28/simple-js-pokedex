67c3a14a18863fe8d07ed2580a9615fe0a4364b3
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-02-03 19:29:03 -0500

    Updating js
    This is to show the understanding of the directions in 1.2. I believe (hopefully) that I've made the Pokemon List as required, calling objects from an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,6 @@
-let name = 'Bulbasaur'; 
-let height = 5; 
-let type = 'Water'; 
-console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
-
-let name = 'Pikachu'; 
-let height = 3; 
-let type = 'Electric'; 
-console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
-
-let name = 'Charizard'; 
-let height = 7; 
-let type = 'Fire'; 
-console.log('My name is ' + name + ' and I\'m ' + height + ' feet tall' + ' and a ' + type + ' Pokemon!');
-
-
-/*
-// array of strings
-let pokemonList = ['Bulbasaur', 'Pikachu', 'Charizard']; 
-console.log(2);
-                   
-           
-// height array, an array of arrays!
-let height = [Array(1), Array(1), Array(1)
-  [3],
-  [2],
-  [5]
+let pokemonList = [
+  { name: 'Pikachu', height: 2, type: 'blue'},
+  { name: 'Bulbasaur', height: 4, type: 'red'},
+  {name: 'Charizard', height: 6, type: 'Fire'}
 ];
-console.log(height);
-
-
-
-// array of objects
-let type = [
-  { type: 'Water'},
-  { type: 'Lightning'},
-  { type: 'Fire'}
-  
-]; 
- console.log(type); 
-
-*/
+console.log(pokemonList);
