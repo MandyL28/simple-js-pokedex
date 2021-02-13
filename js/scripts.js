78421d32e7bfca0506baeadf1debd8e7b7a558e4
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-02-13 08:40:37 -0500

    Updates to JS for 1.3
    Have added loops and conditionals to the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-let pokemonList = [
-  { name: 'Pikachu', height: 2, type: 'blue'},
-  { name: 'Bulbasaur', height: 4, type: 'red'},
-  {name: 'Charizard', height: 6, type: 'Fire'}
-];
-console.log(pokemonList);
+
+
+let pokemonList = [                                                               //instead of personAge, it is now pokemonList
+	{name: 'Pikachu', height: 2, type: 'blue'},         //instead of ages, now it is height and type (need array for type?)
+	{name: 'Bulbasaur', height: 6, type: 'red'}, 
+	{name: 'Charizard', height: 12, type: 'Fire'} 
+    ];
+    
+ for (let i=0; 1 <pokemonList.length; i++){                     //now, how to adjust it for height or type vs age???
+    if (pokemonList[i].height <=6 && pokemonList[i].height >5){
+       document.write(pokemonList[i].name + " is 6ft" + " - that is short but managable");
+    }else if (pokemonList[i].height <3){
+     document.write(pokemonList[i].name + " is 2ft " + " - that's super short"); 
+    }else {
+        documetn.write(pokemonList[i].name + " is 12ft" + " - that is tall!");
+    } 
+ }
+
+    
+    
