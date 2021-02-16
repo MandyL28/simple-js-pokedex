e426e5de3019355c84231d302c97fc9aa4aa1b07
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-02-16 12:14:59 -0500

    Updated script to get rid of spaces
    Too much space rendered my code to be wonky

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 
 
-let pokemonList = [                                                               //instead of personAge, it is now pokemonList
-	{name: 'Pikachu', height: 2, type: 'blue'},         //instead of ages, now it is height and type (need array for type?)
+let pokemonList = [ //instead of personAge, it is now pokemonList
+	{name: 'Pikachu', height: 2, type: 'blue'},//instead of ages, now it is height and type (need array for type?)
 	{name: 'Bulbasaur', height: 6, type: 'red'}, 
 	{name: 'Charizard', height: 12, type: 'Fire'} 
     ];
     
- for (let i=0; 1 <pokemonList.length; i++){                     //now, how to adjust it for height or type vs age???
+ for (let i=0; 1 <pokemonList.length; i++){  //now, how to adjust it for height or type vs age???
     if (pokemonList[i].height <=6 && pokemonList[i].height >5){
        document.write(pokemonList[i].name + " is 6ft" + " - that is short but managable");
-	document.write(<br/>);
+	document.write(br/);
     }else if (pokemonList[i].height <3){
      document.write(pokemonList[i].name + " is 2ft " + " - that's super short"); 
     }else {
