89a7c85255ac739cb2c815814fef8eaa7a509826
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-04-23 07:31:43 -0700

    This is the update to my code. The list seems to be working so far.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-/* 
-
+ 
+/*
 //First part of assignment!!
 
 let pokemonList = [ //instead of personAge, it is now pokemonList
@@ -10,18 +10,17 @@ let pokemonList = [ //instead of personAge, it is now pokemonList
     
  for (let i=0; 1 <pokemonList.length; i++){  //now, how to adjust it for height or type vs age???
     if (pokemonList[i].height <=6 && pokemonList[i].height >5){
-       document.write(pokemonList[i].name + " is 6ft" + " - that is short but managable");
-	document.write(br/);
+       document.write(pokemonList[i].name + " is 6ft" + " - that is short but managable. " + "");
     }else if (pokemonList[i].height <3){
-     document.write(pokemonList[i].name + " is 2ft " + " - that's super short"); 
+     document.write(pokemonList[i].name + " is 2ft " + " - that's super short! "); 
     }else {
         document.write(pokemonList[i].name + " is 12ft" + " - that is tall!");
     } 
- }*/
+ }
+*/
 
 
 /*
-
 //SECOND part of assignment!!!
 
 let pokemonList = [     // Pokémon objects
@@ -29,7 +28,7 @@ let pokemonList = [     // Pokémon objects
   {name: 'Bulbasaur', height: 6, type: 'plant'}, 
   {name: 'Charizard', height: 12, type: 'Fire'} 
 ];
-
+document.write(pokemonList);
 let pokemonList2 = [     // different set of Pokémon objects
   {name: 'Pichu', height: 2, type: 'electric'},
   {name: 'Mewtwo', height: 7, type: 'psychic'}, 
@@ -39,15 +38,16 @@ let pokemonList2 = [     // different set of Pokémon objects
 
 function printArrayDetails(pokemonList){
   for (let i = 0; i <pokemonList.length; i++){
-    // document.write("<p>" + list[i].name + "</p>");
-    // printing list[i]’s other details
-    // ...
+     document.write(list[i].name);
+    // printing list[i] details
+    
   }
 }
-
-printArrayDetails(pokemonList); // executes the function using ‘pokemonList‘ as its input
-printArrayDetails(pokemonList2); // executes the function using ‘pokemonList2‘ as its input
 */
+
+//printArrayDetails(pokemonList); // executes the function using 'pokemonList' as its input
+//printArrayDetails(pokemonList2); // executes the function using 'pokemonList2' as its input
+
 
 //THIRD PART OF ASSIGNMENT
 /*
@@ -87,8 +87,68 @@ var pokemonRepository = (function () {
   };
 })();
 
-console.log( pokemonRepository.getAll() );
+console.log(pokemonRepository.getAll() );
+
 */
 
 
+//---------------
 
+/*
+//THIS IS GOOD START HERRREEEEE
+var pokemonList = [   
+  {
+    name: 'Pikachu',
+    height: 2, 
+    type: 'electric'
+  },
+  {
+    name: 'Bulbasaur', 
+    height: 6, 
+    type: 'plant'
+  },
+  {
+    name: 'Charizard', 
+    height: 12, 
+    type: 'fire'
+  }
+
+]
+
+document.write(pokemonList[0].name)
+document.write(", ")
+document.write(pokemonList[1].name)
+document.write(",")
+document.write(pokemonList[2].name)
+  
+*/
+
+//OK, SO NOWWW......
+var pokemonList = [   
+  {
+    name: 'Pikachu',
+    height: 2, 
+    type: 'electric'
+  },
+  {
+    name: 'Bulbasaur', 
+    height: 6, 
+    type: 'plant'
+  },
+  {
+    name: 'Charizard', 
+    height: 12, 
+    type: 'fire'
+  }
+
+]
+
+for (let i=0; 1 <pokemonList.length; i++){  //now, how to adjust it for height or type vs age???
+      if (pokemonList[i].height <=6 && pokemonList[i].height >5){
+         document.write(pokemonList[i].name + " is 6ft" + " - that's standard. " + "");
+      }else if (pokemonList[i].height <3){
+       document.write(pokemonList[i].name + " is 2ft " + " - that's super short! "); 
+      }else {
+          document.write(pokemonList[i].name + " is 12ft" + " - that is tall!");
+      } 
+   }
