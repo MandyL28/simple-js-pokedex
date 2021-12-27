8e00d557a9df51eb06585ec2a17ec5e47fd4abd1
Mandy L <60361879+MandyL28@users.noreply.github.com>
2021-12-27 14:14:50 -0500

    I am uploading changes (Dec 21)
    These are all the newest changes as of  December 2021

--- js/scripts.js
+++ js/scripts.js
@@ -1,113 +1,19 @@
-
-
-/*
-    //PART 1
-
-var pokemonList = [   
-  {
-    name: 'Pikachu',
-    height: 2, 
-    type: 'electric'
-  },
-  {
-    name: 'Bulbasaur', 
-    height: 6, 
-    type: 'plant'
-  },
-  {
-    name: 'Charizard', 
-    height: 12, 
-    type: 'fire'
-  }
-
-]
-
-document.write(pokemonList[0].name)
-document.write(", ")
-document.write(pokemonList[1].name)
-document.write(",")
-document.write(pokemonList[2].name)
-*/
-
-    //And then:
-
-
-/*
-let pokemonList = [ 
-	{name: 'Pikachu', height: 2, type: 'electric'},       //instead of ages, now it is height and type (need array for type?)
-	{name: 'Bulbasaur', height: 6, type: 'plant'}, 
-	{name: 'Charizard', height: 12, type: 'Fire'} 
-    ];
-    
- for (let i=0; 1 <pokemonList.length; i++){  //now, how to adjust it for height or type vs age???
-    if (pokemonList[i].height <=6 && pokemonList[i].height >5){
-       document.write(pokemonList[i].name + " is 6ft" + " - that is short but managable. " + "");
-    }else if (pokemonList[i].height <3){
-     document.write(pokemonList[i].name + " is 2ft " + " - that's super short! "); 
-    }else {
-        document.write(pokemonList[i].name + " is 12ft" + " - that is tall!");
-    };
- }
-*/
-
-
-
-//OK, so now printing 2 arrays
-
-/*
-let pokemonList = [   
-  {
-    name: 'Pikachu',
-    height: 2, 
-    type: 'electric'
-  },
-  {
-    name: 'Bulbasaur', 
-    height: 6, 
-    type: 'plant'
-  },
-  {
-    name: 'Charizard', 
-    height: 12, 
-    type: 'fire'
-  }
-
+let pokemonList = [     // Pokémon objects
+  {name: 'Pikachu', height: 2, type: 'electric'},
+  {name: 'Bulbasaur', height: 6, type: 'plant'}, 
+  {name: 'Charizard', height: 12, type: 'Fire'} 
 ];
-*/
-/*
-function printArrayDetails(pokemonList){ 
-  for (let i = 0; i < pokemonList.length; i++){
-      document.write("<p>" +pokemonList[i].name + "</p>"); 
-      document.write("<p>" +pokemonList[i].height + "</p>"); 
-      document.write("<p>" +pokemonList[i].type + "</p>")
-     // printing pokemonList[i]'s other details // ... 
-    } 
-  }
-
-  
-let pokemonList2 = [    
+let pokemonList2 = [     // different set of Pokémon objects
   {name: 'Pichu', height: 2, type: 'electric'},
   {name: 'Mewtwo', height: 7, type: 'psychic'}, 
-  {name: 'Meowth', height: 5, type: 'feline'} 	
-	
+  {name: 'Meowth', height: 5, type: 'feline'}   
+  
 ];
-
-function printArrayDetails(pokemonList2){
-  for (let i = 0; i <pokemonList2.length; i++){
-     document.write(list[i].name);
-    // printing list[i] details
-    
+function printArrayDetails(pokemonList){
+  for (let i = 0; i < pokemonList.length; i++){
+     document.write("<p>" + pokemonList[i].name + " is " + " ");
+     document.write(pokemonList[i].height + 'ft.' + " and is type: " + pokemonList[i].type);
   }
 }
-*/
-
-var a = [["<p>" + "Name: Pikachu", "Height:2", "type: electric"], ["<p>" + "Name: Bulbasaur", "Height:6", "type: Grass"], ["<p>" + "Name: Charizard", "Height:2", "type: electric"]];
- 
-for (var i in a) 
-{
-   document.write("row " + i);
-   for (var j in a[i]) 
-     {
-      document.write(" " + a[i][j]);
-     }
-}
+printArrayDetails(pokemonList); // executes the function using ‘pokemonList‘ as its input
+printArrayDetails(pokemonList2); // executes the function using ‘pokemonList2‘ as its input. Oops - replace list[i].name with pokemonList[i].name and add pokemonList[i].height, too. 
